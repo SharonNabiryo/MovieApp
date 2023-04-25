77aa92b1beefc47182c6738206957a279c92c2f4
Sharon N Nabiryo <sharongless37@gmail.com>
2023-04-25 02:06:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,65 @@
 import { useState, useEffect } from "react";
 
-import './App.css';
-import SearchIcon from './search.svg'
-import MovieCard from './MovieCard'
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 // c7d7f198
 
-const apiUrl = 'http://www.omdbapi.com?apikey=c7d7f198';
+const apiUrl = "http://www.omdbapi.com?apikey=c7d7f198";
 
-const movie1 = {
-    "Poster": "N/A",
-    "Title": "Spiderman",
-    "Type": "movie",
-    // "Year": "2010",
-    "imdbID": "tt1785572"
-};
-
+// const movie1 = {
+//     "Poster": "N/A",
+//     "Title": "Spiderman",
+//     "Type": "movie",
+//     // "Year": "2010",
+//     "imdbID": "tt1785572"
+// };
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('')
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${apiUrl}&s=${title}`);
-        const data = await response.json();
+    setMovies(data.Search);
+    // we first console data then saw the name of the array and added it)
+  };
 
-        setMovies(data.Search)
-        // we first console data then saw the name of the array and added it)
-    }
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
 
-    useEffect(() => {
-        searchMovies('spiderman')
-    }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
-
-    return (
-        <div className="app">
-            <h1>MovieLand</h1>
-
-            <div className="search">
-                <input placeholder="search for movies"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                // searchMovies is to call the function above so that our state changes when rendered
-                />
-            </div>
-            {
-                movies?.length > 0
-                    ? (
-                        <div className="container">
-                            {movies.map((movie) => (
-                                < MovieCard movie={movie} />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
-
-                    )
-            }
-
-
-
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+          // searchMovies is to call the function above so that our state changes when rendered
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-
-    )
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
